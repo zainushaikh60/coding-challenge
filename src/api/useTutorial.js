fae920c8f9122f4216c0cc5a3e356285f45b40ea
zainushaikh60 <zainushaikh60@gmail.com>
2022-02-26 15:36:24 +0500

    updated

--- src/api/useTutorial.js
+++ src/api/useTutorial.js
@@ -8,28 +8,28 @@ const useTutorial = () => {
       stepsLoading: false,
     }));
 
-    try {
-      const response = await fetch(
-        "https://knwrn-frontend-challenge.free.beeceptor.com"
-      );
-      const data = await response.json();
+    // try {
+    //   const response = await fetch(
+    //     "https://knwrn-frontend-challenge.free.beeceptor.com"
+    //   );
+    //   const data = await response.json();
 
-      if (data) {
-        localStorage.setItem(
-          "tutorials",
-          JSON.stringify([...data.tutorial.steps])
-        );
-        useGlobalState.setState((state) => ({
-          steps: [...state.steps, ...data.tutorial.steps],
-          stepsLoading: false,
-        }));
-      }
-    } catch (error) {
-      useGlobalState.setState({
-        stepsLoading: false,
-      });
-      console.log(error);
-    }
+    //   if (data) {
+    //     localStorage.setItem(
+    //       "tutorials",
+    //       JSON.stringify([...data.tutorial.steps])
+    //     );
+    //     useGlobalState.setState((state) => ({
+    //       steps: [...state.steps, ...data.tutorial.steps],
+    //       stepsLoading: false,
+    //     }));
+    //   }
+    // } catch (error) {
+    //   useGlobalState.setState({
+    //     stepsLoading: false,
+    //   });
+    //   console.log(error);
+    // }
   };
 
   return {
